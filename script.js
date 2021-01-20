80b59f0ed6a153694fe2285ea720e7a7e5c4b822
Gabriel Holmkvist <gabbeholman99@gmail.com>
2021-01-20 11:15:28 +0100

    Limited decimals in monthly cost to 2

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 let sumSlider = document.getElementById("sumSlider");
-let sumOutput = document.getElementById("summa");
 let yearSlider = document.getElementById("yearSlider");
-let yearOutput = document.getElementById("year");
 let totalCost = document.getElementById("totalCost");
 let monthlyCost = document.getElementById("monthlyCost")
 let sumFormOutput = document.getElementById("sum");
 let yearFormOutput = document.getElementById("yearForm");
-
-let interest = 
 
 sumFormOutput.value = sumSlider.value;
 sumSlider.value = sumFormOutput.value;
@@ -15,7 +11,8 @@ sumSlider.value = sumFormOutput.value;
 sumSlider.oninput = function(){
     sumFormOutput.value = sumSlider.value;
     totalCost.innerHTML = (sumSlider.value * 1.054);
-    monthlyCost.innerHTML = (sumSlider.value * 1.054) / (yearSlider.value * 12);
+    let monthlyCostSum = (sumSlider.value * 1.054) / (yearSlider.value * 12);
+    monthlyCost.innerHTML = (monthlyCostSum * 1).toFixed(2);
 }
 
 yearFormOutput.value = yearSlider.value;
@@ -24,17 +21,21 @@ yearSlider.value = yearFormOutput.value;
 yearSlider.oninput = function(){
     yearFormOutput.value = yearSlider.value;
     totalCost.innerHTML = (sumSlider.value * 1.054);
-    monthlyCost.innerHTML = (sumSlider.value * 1.054) / (yearSlider.value * 12);
+    let monthlyCostSum = (sumSlider.value * 1.054) / (yearSlider.value * 12);
+    monthlyCost.innerHTML = (monthlyCostSum * 1).toFixed(2);
 }
 
 sumFormOutput.oninput = function(){
     sumSlider.value = sumFormOutput.value;
     totalCost.innerHTML = (sumSlider.value * 1.054);
-    monthlyCost.innerHTML = (sumSlider.value * 1.054) / (yearSlider.value * 12);
+    let monthlyCostSum = (sumSlider.value * 1.054) / (yearSlider.value * 12);
+    monthlyCost.innerHTML = (monthlyCostSum * 1).toFixed(2);
 }
 
 yearFormOutput.oninput = function(){
     yearSlider.value = yearFormOutput.value;
     totalCost.innerHTML = (sumSlider.value * 1.054);
-    monthlyCost.innerHTML = (sumSlider.value * 1.054) / (yearSlider.value * 12);
+    let monthlyCostSum = (sumSlider.value * 1.054) / (yearSlider.value * 12);
+    // Multiplicerar value på summan med 1 så att det blir en Int men inte ändrar värde //
+    monthlyCost.innerHTML = (monthlyCostSum * 1).toFixed(2);
 }
