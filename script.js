0d074101e33fb7252d9c9ba8d167d973472e05cb
Gabriel Holmkvist <gabbeholman99@gmail.com>
2021-01-20 09:56:23 +0100

    fixed so the slider and forminput syncs

--- script.js
+++ script.js
@@ -2,15 +2,39 @@ let sumSlider = document.getElementById("sumSlider");
 let sumOutput = document.getElementById("summa");
 let yearSlider = document.getElementById("yearSlider");
 let yearOutput = document.getElementById("year");
+let totalCost = document.getElementById("totalCost");
+let monthlyCost = document.getElementById("monthlyCost")
+let sumFormOutput = document.getElementById("sum");
+let yearFormOutput = document.getElementById("yearForm");
 
-sumOutput.innerHTML = sumSlider.value;
+let interest = 
+
+sumFormOutput.value = sumSlider.value;
+sumSlider.value = sumFormOutput.value;
 
 sumSlider.oninput = function(){
-    sumOutput.innerHTML = this.value
+    sumFormOutput.value = sumSlider.value;
+    totalCost.innerHTML = (sumSlider.value * 1.054);
+    monthlyCost.innerHTML = (sumSlider.value * 1.054) / (yearSlider.value * 12);
 }
 
-yearOutput.innerHTML = yearSlider.value;
+yearFormOutput.value = yearSlider.value;
+yearSlider.value = yearFormOutput.value;
 
 yearSlider.oninput = function(){
-    yearOutput.innerHTML = this.value;
+    yearFormOutput.value = yearSlider.value;
+    totalCost.innerHTML = (sumSlider.value * 1.054);
+    monthlyCost.innerHTML = (sumSlider.value * 1.054) / (yearSlider.value * 12);
+}
+
+sumFormOutput.oninput = function(){
+    sumSlider.value = sumFormOutput.value;
+    totalCost.innerHTML = (sumSlider.value * 1.054);
+    monthlyCost.innerHTML = (sumSlider.value * 1.054) / (yearSlider.value * 12);
+}
+
+yearFormOutput.oninput = function(){
+    yearSlider.value = yearFormOutput.value;
+    totalCost.innerHTML = (sumSlider.value * 1.054);
+    monthlyCost.innerHTML = (sumSlider.value * 1.054) / (yearSlider.value * 12);
 }
